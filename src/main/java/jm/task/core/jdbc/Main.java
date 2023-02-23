51bad7b3e9501c1955d1d613fa4eac76fe850538
Arman <arman458792@gmail.com>
2023-02-23 22:24:36 +0600

    main and test codes apply

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
+public class Main {
+    public static void main(String[] args) {
+        UserService us = new UserServiceImpl();
+        User user1 = new User("Andre", "Smith", (byte) 10);
+        User user2 = new User("Andy", "Smith", (byte) 7);
+        User user3 = new User("Edie", "Smith", (byte) 14);
+        User user4 = new User("Walter", "Smith", (byte) 18);
+        us.createUsersTable();
+        us.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        us.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        us.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        us.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        us.getAllUsers();
+        us.cleanUsersTable();
+        us.dropUsersTable();
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,105 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UserDaoJDBCImpl implements UserDao {
+
+
+
+    private final Connection connection = Util.getConnection();
+
+    public UserDaoJDBCImpl() {
+
+    }
+
+    public void createUsersTable() {
+
+        String createTableCom = "CREATE TABLE IF NOT EXISTS `mydbtest`.`users` (\n" +
+                "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                "  `name` VARCHAR(45) NOT NULL,\n" +
+                "  `lastName` VARCHAR(45) NOT NULL,\n" +
+                "  `age` VARCHAR(45) NOT NULL,\n" +
+                "  PRIMARY KEY (`id`),\n" +
+                "  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)\n" +
+                "ENGINE = InnoDB\n" +
+                "DEFAULT CHARACTER SET = utf8;";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(createTableCom);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void dropUsersTable() {
+
+        String removeTable = "DROP TABLE IF EXISTS users";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(removeTable);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+
+        String sql = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setString(2, lastName);
+            pstmt.setByte(3, age);
+            pstmt.executeUpdate();
+            System.out.printf("User с именем - %s добавлен в базу данных\n", name);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void removeUserById(long id) {
+
+        String removeTable = "delete from users where id = ?";
+        try (PreparedStatement ps = connection.prepareStatement(removeTable)) {
+            ps.setLong(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<User> getAllUsers() {
+
+        List<User> listOfUsers = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+
+        try (Statement statement = connection.createStatement()) {
+            ResultSet result = statement.executeQuery(sql);
+            while (result.next()) {
+                User user = new User();
+                user.setId(result.getLong("id"));
+                user.setName(result.getString("name"));
+                user.setLastName(result.getString("lastName"));
+                user.setAge((byte) result.getInt("age"));
+                listOfUsers.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(listOfUsers);
+        return listOfUsers;
+    }
+
+    public void cleanUsersTable() {
+        String cleanUp = "DELETE FROM users";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(cleanUp);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
