cf23d28dfcada2a2250e53041215cff89f04d195
Arman <arman458792@gmail.com>
2023-02-28 00:32:20 +0600

    Добавил аннотации для dao и service слоев.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,8 @@
-package jm.task.core.jdbc.service;
+package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 
 public class Main {
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -3,36 +3,41 @@ package jm.task.core.jdbc.service;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
+
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private final Util util = new Util();
 
     private final UserDao udji = new UserDaoJDBCImpl();
 
+    @Override
     public void createUsersTable() {
         udji.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
         udji.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         udji.saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
         udji.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
         return udji.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
         udji.cleanUsersTable();
     }
